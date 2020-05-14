1606441722340ae0c7be5be9c8210098e8d8b858
Emil Hansen <emilgth@gmail.com>
2020-05-14 12:23:34 +0200

    app fronunt endd

--- main.py
+++ main.py
@@ -5,13 +5,25 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
 from keras import layers
 from keras import regularizers
+import tensorflow as tf
+
+physical_devices = tf.config.experimental.list_physical_devices('GPU')
+tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
 PATH = './PokemonData'
 data_dir = pathlib.Path(PATH)
 
 CLASS_NAMES = np.array([item.name for item in data_dir.glob('*')])
 
-IDG = ImageDataGenerator(rescale=1. / 255, validation_split=0.2, )
+IDG = ImageDataGenerator(rescale=1. / 255,
+                         rotation_range=40,
+                         width_shift_range=0.2,
+                         height_shift_range=0.2,
+                         shear_range=0.2,
+                         zoom_range=0.2,
+                         horizontal_flip=True,
+                         fill_mode='nearest',
+                         validation_split=0.2, )
 
 train_data = IDG.flow_from_directory(PATH, target_size=(224, 224), classes=list(CLASS_NAMES),
                                      subset='training')
@@ -26,16 +38,6 @@ model.add(layers.Dropout(.25))
 model.add(layers.MaxPooling2D(pool_size=(2, 2)))
 
 model.add(layers.Conv2D(16, (4, 4)))
-model.add(layers.Activation('relu'))
-model.add(layers.Dropout(.25))
-model.add(layers.MaxPooling2D(pool_size=(2, 2)))
-
-model.add(layers.Conv2D(32, (4, 4)))
-model.add(layers.Activation('relu'))
-model.add(layers.Dropout(.25))
-model.add(layers.MaxPooling2D(pool_size=(2, 2)))
-
-model.add(layers.Conv2D(32, (4, 4)))
 model.add(layers.Activation('relu'))
 model.add(layers.Dropout(.25))
 model.add(layers.MaxPooling2D(pool_size=(2, 2)))
--- predictions.py
+++ predictions.py
@@ -1,17 +1,24 @@
 import pathlib
+
+from keras import Sequential
 from keras.models import load_model
 from keras.preprocessing.image import ImageDataGenerator
 from keras.utils.np_utils import to_categorical
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+
+# physical_devices = tf.config.experimental.list_physical_devices('GPU')
+# tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
 import cv2
-
 import numpy as np
 
-PATH = './PokemonData'
+PATH = './pkmn'
 data_dir = pathlib.Path(PATH)
-
-CLASS_NAMES = np.array([item.name for item in data_dir.glob('*')])
-print(CLASS_NAMES)
+# CLASS_NAMES = np.array([item.name for item in data_dir.glob('*')])
+CLASS_NAMES = ['bilasba', 'charmmand', 'pciak', 'squite']
+# print(CLASS_NAMES)
 
 IDG = ImageDataGenerator(rescale=1. / 255, validation_split=0.2, )
 
@@ -21,17 +28,17 @@ train_data = IDG.flow_from_directory(PATH, target_size=(224, 224), classes=list(
 validation_data = IDG.flow_from_directory(PATH, target_size=(224, 224), classes=list(CLASS_NAMES),
                                           subset='validation')
 
-model = load_model('./saved_model')
+model = load_model('./saved_model2_val_loss_0.1268_val_accuracy097')
 model.compile(loss='categorical_crossentropy',
               optimizer='adam',
               metrics=['accuracy'])
 
-img = cv2.imread('./PokemonData/Voltorb/aabfc0156f674322ba27b2e977d9d787.jpg')
-img = cv2.resize(img, (224, 224))
-img = np.reshape(img, [1, 224, 224, 3])
-classes = model.predict_classes(img)
 
-print(train_data.filenames)
-
-
-
+def predict(filepath):
+    img = cv2.imread(filepath)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = cv2.resize(img, (224, 224))
+    img = np.reshape(img, [1, 224, 224, 3])
+    classes = model.predict_proba(img, batch_size=None, verbose=True)
+    # return CLASS_NAMES[np.argmax(classes)]
+    return classes[0]
