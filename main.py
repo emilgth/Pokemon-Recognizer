0f487965cd5b94898477c39886908dc8e05c2b95
iosefstalin <jelleholm91@gmail.com>
2020-05-14 14:14:45 +0200

    added histogram to model training, & added barchart to predictions

--- main.py
+++ main.py
@@ -4,26 +4,16 @@ import pathlib
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
 from keras import layers
+from keras.layers import BatchNormalization
 from keras import regularizers
-import tensorflow as tf
+import matplotlib.pyplot as plt
 
-physical_devices = tf.config.experimental.list_physical_devices('GPU')
-tf.config.experimental.set_memory_growth(physical_devices[0], True)
-
-PATH = './PokemonData'
+PATH = './Starters'
 data_dir = pathlib.Path(PATH)
 
 CLASS_NAMES = np.array([item.name for item in data_dir.glob('*')])
 
-IDG = ImageDataGenerator(rescale=1. / 255,
-                         rotation_range=40,
-                         width_shift_range=0.2,
-                         height_shift_range=0.2,
-                         shear_range=0.2,
-                         zoom_range=0.2,
-                         horizontal_flip=True,
-                         fill_mode='nearest',
-                         validation_split=0.2, )
+IDG = ImageDataGenerator(rescale=1. / 255, validation_split=0.2, )
 
 train_data = IDG.flow_from_directory(PATH, target_size=(224, 224), classes=list(CLASS_NAMES),
                                      subset='training')
@@ -31,10 +21,11 @@ train_data = IDG.flow_from_directory(PATH, target_size=(224, 224), classes=list(
 validation_data = IDG.flow_from_directory(PATH, target_size=(224, 224), classes=list(CLASS_NAMES),
                                           subset='validation')
 
+
 model = Sequential()
 model.add(layers.Conv2D(8, (4, 4), input_shape=(224, 224, 3)))
 model.add(layers.Activation('relu'))
-model.add(layers.Dropout(.25))
+# model.add(layers.Dropout(.25))
 model.add(layers.MaxPooling2D(pool_size=(2, 2)))
 
 model.add(layers.Conv2D(16, (4, 4)))
@@ -44,13 +35,35 @@ model.add(layers.MaxPooling2D(pool_size=(2, 2)))
 
 model.add(layers.Conv2D(32, (4, 4)))
 model.add(layers.Activation('relu'))
-model.add(layers.Dropout(.25))
+# model.add(layers.Dropout(.25))
 model.add(layers.MaxPooling2D(pool_size=(2, 2)))
 
+# model.add(layers.Conv2D(32, (4, 4)))
+# model.add(layers.Activation('relu'))
+# # model.add(layers.Dropout(.25))
+# # model.add(layers.MaxPooling2D(pool_size=(2, 2)))
+
+# model.add(layers.Conv2D(32, (4, 4)))
+# model.add(layers.Activation('relu'))
+# # model.add(layers.Dropout(.25))
+# model.add(layers.MaxPooling2D(pool_size=(2, 2)))
+
 model.add(layers.Flatten())
-model.add(layers.Dense(256, activation='relu', kernel_regularizer=regularizers.l2(0.0001)))
+model.add(layers.Dense(256, activation='tanh', kernel_regularizer=regularizers.l2(0.0001)))
 model.add(layers.Dropout(.5))
 model.add(layers.Dense(len(CLASS_NAMES), activation='softmax'))
 
 model.compile(optimizer='adam', loss=keras.losses.categorical_crossentropy, metrics=['accuracy'])
-model.fit(train_data, epochs=10, validation_data=validation_data)
+hist = model.fit(train_data, epochs=6, validation_data=validation_data)
+
+plt.style.use('fivethirtyeight')
+plt.figure(figsize=(14,14))
+plt.plot(hist.history['accuracy'],label='accuracy',color='green')
+plt.plot(hist.history['val_accuracy'],label='val_accuracy',color='blue')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.yticks(np.arange(0, 1, step=0.04))
+plt.show()
+
+
+model.save(filepath='./saved_model_2')
--- predictions.py
+++ predictions.py
@@ -1,23 +1,17 @@
 import pathlib
-
-from keras import Sequential
 from keras.models import load_model
 from keras.preprocessing.image import ImageDataGenerator
 from keras.utils.np_utils import to_categorical
 import matplotlib.pyplot as plt
-import tensorflow as tf
-
-
-# physical_devices = tf.config.experimental.list_physical_devices('GPU')
-# tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
 import cv2
+
 import numpy as np
 
-PATH = './pkmn'
+PATH = 'Starters'
 data_dir = pathlib.Path(PATH)
-# CLASS_NAMES = np.array([item.name for item in data_dir.glob('*')])
-CLASS_NAMES = ['bilasba', 'charmmand', 'pciak', 'squite']
+
+CLASS_NAMES = np.array([item.name for item in data_dir.glob('*')])
 # print(CLASS_NAMES)
 
 IDG = ImageDataGenerator(rescale=1. / 255, validation_split=0.2, )
@@ -28,17 +22,46 @@ train_data = IDG.flow_from_directory(PATH, target_size=(224, 224), classes=list(
 validation_data = IDG.flow_from_directory(PATH, target_size=(224, 224), classes=list(CLASS_NAMES),
                                           subset='validation')
 
-model = load_model('./saved_model2_val_loss_0.1268_val_accuracy097')
+model = load_model('./saved_model')
 model.compile(loss='categorical_crossentropy',
               optimizer='adam',
               metrics=['accuracy'])
 
+img = cv2.imread('./bulb.jpg')
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+img = cv2.resize(img, (224, 224))
+img = np.reshape(img, [1, 224, 224, 3])
+# classes = model.predict_classes(train_data, batch_size=None)
+classes = model.predict_classes(img)
+prediction = model.predict_proba(img)
+print(prediction[0])
 
-def predict(filepath):
-    img = cv2.imread(filepath)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    img = cv2.resize(img, (224, 224))
-    img = np.reshape(img, [1, 224, 224, 3])
-    classes = model.predict_proba(img, batch_size=None, verbose=True)
-    # return CLASS_NAMES[np.argmax(classes)]
-    return classes[0]
+print(classes[0])
+
+def plot_value_array(i, predictions_array, true_label):
+    predictions_array, true_label = predictions_array, true_label[i]
+    plt.grid(False)
+    plt.xticks(range(4))
+    plt.yticks([])
+    thisplot = plt.bar(range(4), predictions_array, color="#777777")
+    plt.ylim([0, 1])
+    predicted_label = np.argmax(predictions_array)
+
+    # thisplot[true_label].set_color('blue')
+
+plot_value_array(0, prediction[0], CLASS_NAMES)
+
+
+
+
+# for guess, real in zip(classes, train_data.filenames):
+#     print(CLASS_NAMES[guess], real)
+
+print(CLASS_NAMES[classes[0]])
+
+plt.show()
+
+# print(train_data.filenames)
+
+
+
